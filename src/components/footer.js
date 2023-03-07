4349c3eb03c2f59c25866ebb681514a536592017
isbvah <isbahtaqweem@gmail.com>
2023-03-07 19:30:36 +0530

    footer.js added

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+const FooterWrapper = styled.footer`
+  text-align: center;
+  padding: 1rem;
+`
+
+const Footer = () => <FooterWrapper>&copy; 2023 CERN HSF</FooterWrapper>
+
+export default Footer
+
